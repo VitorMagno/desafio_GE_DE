435b7b50562ddbd35be2c53fdd1539a5692082c7
Vitor Magno C. de Gouveia <45520686+VitorMagno@users.noreply.github.com>
2025-06-23 15:11:05 -0300

    scripts para automatizar o compose up e down

---
+++ docker/down.ps1
@@ -0,0 +1,2 @@
+docker compose -f docker-compose-airflow.yaml down -v --remove-orphans
+docker compose -f compose.yml down -v --remove-orphans
---
+++ docker/init.ps1
@@ -0,0 +1,2 @@
+docker compose -f docker-compose-airflow.yaml up -d
+docker compose -f compose.yml up -d
