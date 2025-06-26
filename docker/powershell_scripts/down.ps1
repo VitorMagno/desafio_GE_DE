9e28dc38fda486a176b76a85c2762b0eb12fc542
Vitor Magno C. de Gouveia <45520686+VitorMagno@users.noreply.github.com>
2025-06-25 23:16:57 -0300

    patternize

--- docker/powershell_scripts/down.ps1
+++ docker/powershell_scripts/down.ps1
@@ -1,2 +1,3 @@
-docker compose -f ..\airflow\docker-compose-airflow.yaml down -v --remove-orphans
+docker compose -f ..\airflow\compose.yaml down -v --remove-orphans
 docker compose -f ..\minio_e_metastore\compose.yml down -v --remove-orphans
+docker compose -f ..\spark_jupyter\compose.yml down -v --remove-orphans
--- docker/powershell_scripts/init.ps1
+++ docker/powershell_scripts/init.ps1
@@ -1,3 +1,4 @@
-docker compose -f ..\airflow\docker-compose-airflow.yaml up -d
+docker compose -f ..\airflow\compose.yaml up -d
 docker compose -f ..\minio_e_metastore\compose.yml up -d
 ..\minio_e_metastore\windows_bash\first_setup.ps1
+docker compose -f ..\spark_jupyter\compose.yml
